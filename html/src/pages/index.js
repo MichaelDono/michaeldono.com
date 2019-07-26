6a1284bd8d9a3c9c855df3726364d4dd1d8ba062
Michael Dono <michael@michaeldono.com>
2019-07-26 20:07:14 +0100

    Additional test change

--- html/src/pages/index.js
+++ html/src/pages/index.js
@@ -28,7 +28,7 @@ const IndexPage = () => (
         <div>
           <h1 className={styles.header}>Michael Dono</h1>
           <ul>
-            <li>Computer Science Undergraduate at Newcastle University.</li>
+            <li>Computer Science Undergraduate at Newcastle.</li>
             <li>Intelligent Infrastucture at Network Rail.</li>
             <li>Former Child.</li>
           </ul>
