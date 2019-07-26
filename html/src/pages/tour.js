bdd6d6bac2f7fdd9a411b1139aa42380d1547dc4
Michael Dono <michael@michaeldono.com>
2019-07-26 20:35:55 +0100

    Updated Helix info

--- html/src/pages/tour.js
+++ html/src/pages/tour.js
@@ -278,7 +278,7 @@ export default ({ data }) => (
             <span>The USB is part of the Helix site. This area of Newcastle has a long history. The entire site used to be used to brew Newcastle Brown Ale, and before that it was a coal mine.
                 Tyne Brewery closed in 2005, after which the area has changed drastically.</span><br/>
             <span>This photo shows the former bottling plant (now The Core, opposite the People's Kitchen). The Core opened in 2014, the first part of what was then Science Central.
-                The Key opened in 2016 just behind it. The Urban Sciences Building opened in 2017, and The Biosphere is opening this month.</span>
+                The Key opened in 2016 just behind it. The Urban Sciences Building opened in 2017.</span>
           </Col>
         </Row>
       </Row>
