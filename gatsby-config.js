b480a4d99a6c9c18244b22164c0588d72277d0b7
Michael Dono <michael@michaeldono.com>
2019-08-08 12:45:57 +0100

    Removed unused dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,5 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     'gatsby-plugin-netlify-identity-widget',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
