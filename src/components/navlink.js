56f0b3289dbddd8e994bebc74139cc0ce5ca81c3
Michael Dono <michael@michaeldono.com>
2019-08-09 15:10:27 +0100

    tried git lfs checkout

--- src/components/navlink.js
+++
@@ -1,8 +0,0 @@
-import { Link } from "gatsby"
-import React from "react"
-
-import styles from './navlink.module.css'
-
-export default (props) => (
-    <Link {...props} activeClassName={styles.active} className={styles.link} to={props.linkTo}>{props.children}</Link>
-  )
